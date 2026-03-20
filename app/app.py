48c3405d6a14f80b2bbcbe1b7ac935c56d686ad2
pranav <psoniradhe177@gmail.com>
2026-03-20 09:42:27 +0530

    modified app.py to remove secret manager configuration as it's not working at all

--- app/app.py
+++ app/app.py
@@ -16,34 +16,10 @@ def home():
     })
 
 
-def get_secret():
-    secret_name = "db-secret"
-    region_name = "ap-south-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise Exception
-
-    secret_dict = json.loads(get_secret_value_response['SecretString'])
-    return secret_dict
-
-
 @app.route("/secret")
 def secret():
     return jsonify({
-        "db_password": get_secret()["DB_PASSWORD"]
+        "db_password": os.getenv("DB_PASSWORD", "not set")
     })
 
 
